982660de3cf83a70d9cbca938601305832243dee
Pierre-Cinart <cinartpierre@gmail.com>
2023-03-16 09:48:30 +0100

    recupération des elements du dom sur le fichier app.js

---
+++ Js/app.js
@@ -0,0 +1,8 @@
+// / creer un menu 
+// / afficher 4 images de lecture de son 
+// / afficher le nombre de lettres correspondants au mot mystère
+// / afficher les lettres disponibles
+// / determiner l affichage en pensant mobile first
+
+// RECUPERATION ELEMENTS DU DOM
+const menu = document.querySelector('#menu');
