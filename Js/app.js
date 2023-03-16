0fc9ec63f64c7a7bf0b5ee1814379bcc274d6819
Pierre-Cinart <cinartpierre@gmail.com>
2023-03-16 12:20:49 +0100

    debut de mise en place css mobile first

--- Js/app.js
+++ Js/app.js
@@ -2,7 +2,12 @@
 // / afficher 4 images de lecture de son 
 // / afficher le nombre de lettres correspondants au mot mystère
 // / afficher les lettres disponibles
-// / determiner l affichage en pensant mobile first
+// / determiner l affichage en pensant mobile first // test 100% POUR TOUS 
 
 // RECUPERATION ELEMENTS DU DOM
+const infoBar = document.querySelector('#infoBar');
 const menu = document.querySelector('#menu');
+const _images = document.querySelector('#_images');
+const letterEmpty = document.querySelector('#letterEmpty');
+const letters = document.querySelector('#letters');
+
